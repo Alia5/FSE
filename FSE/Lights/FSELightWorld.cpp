4fa79efb63cbcaa05dae9f0915343c4b03a967b5
Peter Repukat <orly.alia5@gmail.com>
2017-03-01 21:40:21 +0100

    Lights: Hack bloom somehow into this thing...

--- FSE/Lights/FSELightWorld.cpp
+++ FSE/Lights/FSELightWorld.cpp
@@ -3,6 +3,8 @@
 
 #include <rttr/registration>
 
+#include <SFML/Graphics.hpp>
+
 namespace fse
 {
 	FSELightWorld::FSELightWorld(Scene* scene) : FSELightWorld(scene, sf::Vector2f(0, 0))
@@ -14,6 +16,12 @@ namespace fse
 	{
 		setZOrder(255);
 		light_system_ = std::make_unique<ltbl::LightSystem>(normal_texture_, specular_texture_, true);
+		bloom_texture_.create(1920, 1080);
+		b_texture_.create(1920, 1080);
+
+		bloom_shader_.loadFromMemory(bloom_shader_str_, sf::Shader::Fragment);
+		gauss_blur_shader_.loadFromMemory(gauss_blur_shader_str_, sf::Shader::Fragment);
+		blend_shader_.loadFromMemory(blend_shader_str_, sf::Shader::Fragment);
 	}
 
 	FSELightWorld::~FSELightWorld()
@@ -34,6 +42,60 @@ namespace fse
 			normal_texture_.display();
 			specular_texture_.display();
 			light_system_->render(target);
+
+			if (bloom_)
+			{
+				bloom_texture_.clear(sf::Color::Black);
+
+				sf::View view = scene_->getRenderTarget()->getView();
+
+				if (sf::RenderWindow* window = dynamic_cast<sf::RenderWindow*>(&target))
+				{
+
+					b_texture_.update(*window);
+
+					sf::Sprite sprite = sf::Sprite(b_texture_);
+					//sprite.setPosition(view.getCenter() - view.getSize() / 2.f);
+
+					bloom_shader_.setUniform("currTex", sf::Shader::CurrentTexture);
+
+					bloom_texture_.draw(sprite, &bloom_shader_);
+					bloom_texture_.display();
+
+					gauss_blur_shader_.setUniform("currTex", sf::Shader::CurrentTexture);
+					gauss_blur_shader_.setUniform("texSize", view.getSize());
+
+					bool horizontal = false;
+					for (int i = 0; i < 4; i++)
+					{
+						horizontal = !horizontal;
+						gauss_blur_shader_.setUniform("horizontal", horizontal);
+						bloom_texture_.draw(sf::Sprite(bloom_texture_.getTexture()), &gauss_blur_shader_);
+						bloom_texture_.display();
+					}
+
+
+					sprite = sf::Sprite(bloom_texture_.getTexture());
+					sprite.setPosition(view.getCenter() - view.getSize() / 2.f);
+
+					//blend_shader_.setUniform("exposure", exposure_);
+					//blend_shader_.setUniform("bloomBlur", sf::Shader::CurrentTexture);
+					//blend_shader_.setUniform("scene", b_texture_);
+
+
+					//window->draw(sprite, &blend_shader_);
+					//window->draw(sprite);
+
+					window->draw(sprite, sf::BlendAdd);
+
+				}
+				else if (false)
+				{
+					
+				}
+
+			}
+
 		}
 	}
 
@@ -62,11 +124,18 @@ namespace fse
 	{
 		if (lighting_)
 		{
+			if (bloom_texture_.getSize() != scene_->getRenderTarget()->getSize())
+			{
+				bloom_texture_.create(scene_->getRenderTarget()->getSize().x, scene_->getRenderTarget()->getSize().y);
+				b_texture_.create(scene_->getRenderTarget()->getSize().x, scene_->getRenderTarget()->getSize().y);
+			}
+
 			sf::View view = scene_->getRenderTarget()->getView();
-			normal_texture_.clear(sf::Color(128u, 128u, 255u));
-			specular_texture_.clear(sf::Color::Black);
 			normal_texture_.setView(view);
 			specular_texture_.setView(view);
+			//bloom_texture_.setView(view);
+			normal_texture_.clear(sf::Color(128u, 128u, 255u));
+			specular_texture_.clear(sf::Color::Black);
 		}
 	}
 
@@ -110,6 +179,8 @@ RTTR_REGISTRATION
 
 	registration::class_<FSELightWorld>("fse::FSELightWorld")
 	.property("lighting_", &FSELightWorld::lighting_)
+	.property("bloom_", &FSELightWorld::bloom_)
+	.property("exposure_", &FSELightWorld::exposure_)
 	.property("ambient_color_", &FSELightWorld::getAmbientColor, &FSELightWorld::setAmbientColor)
 	;
 }
--- FSE/Lights/FSELightWorld.h
+++ FSE/Lights/FSELightWorld.h
@@ -40,13 +40,92 @@ namespace fse
 		sf::RenderTexture normal_texture_;
 		sf::RenderTexture specular_texture_;
 
+		sf::RenderTexture bloom_texture_;
+		sf::Texture b_texture_;
+
 		std::unique_ptr<ltbl::LightSystem> light_system_;
 		bool lighting_ = true;
 
+		bool bloom_ = true;
+		float exposure_ = 1.0f;
+
 		ltbl::LightDirectionEmission* sun_ = nullptr;
 
+		sf::Shader bloom_shader_;
+		sf::Shader gauss_blur_shader_;
+		sf::Shader blend_shader_;
 
 		RTTR_ENABLE(fse::FSEObject)
 		RTTR_REGISTRATION_FRIEND
+
+
+		const std::string bloom_shader_str_ = "" \
+		"uniform sampler2D currTex;" \
+		"" \
+		"void main()" \
+		"{" \
+		"	vec2 coord = gl_TexCoord[0].xy;" \
+		"	vec4 frColor = texture2D(currTex, coord);" \
+		"	float brightness = dot(frColor.rgb, vec3(1.2126, 1.7152, 1.0722));" \
+		"	if (brightness > 1.8)" \
+		"	{" \
+		"		gl_FragColor = vec4(frColor.rgb, 1.0);" \
+		"	}" \
+		"	else" \
+		"	{" \
+		"		gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);" \
+		"	}" \
+		"}";
+		const std::string gauss_blur_shader_str_ = "" \
+		"uniform sampler2D currTex;" \
+		"uniform bool horizontal;" \
+		"uniform vec2 texSize; " \
+		"const float pi = 3.14159265;" \
+		"" \
+		"void main() {" \
+		"	const float numBlurPixelsPerSide = 17;" \
+		"	vec2  blurMultiplyVec = vec2(0.0, 1.0);" \
+		"	float sigma = 15.243;" \
+		"	if (horizontal)" \
+		"	{" \
+		"		blurMultiplyVec = vec2(1.0, 0.0);" \
+		"	}" \
+		"	float blurSize = 1.0 / texSize.y;" \
+		"	if (horizontal)" \
+		"	{" \
+		"		blurSize = 1.0 / texSize.x;" \
+		"	}" \
+		"	vec3 incrementalGaussian;" \
+		"	incrementalGaussian.x = 1.0f / (sqrt(2.0 * pi) * sigma);" \
+		"	incrementalGaussian.y = exp(-0.5 / (sigma * sigma));" \
+		"	incrementalGaussian.z = incrementalGaussian.y * incrementalGaussian.y;" \
+		"	vec4 avgValue = vec4(0.0, 0.0, 0.0, 0.0);" \
+		"	float coefficientSum = 0.0;" \
+		"	avgValue += texture2D(currTex, gl_TexCoord[0].xy) * incrementalGaussian.x;" \
+		"	coefficientSum += incrementalGaussian.x;" \
+		"	incrementalGaussian.xy *= incrementalGaussian.yz;" \
+		"	for (float i = 1.0; i <= numBlurPixelsPerSide; i++) {" \
+		"		avgValue += texture2D(currTex, gl_TexCoord[0].xy - i * blurSize *" \
+		"			blurMultiplyVec) * incrementalGaussian.x;" \
+		"		avgValue += texture2D(currTex, gl_TexCoord[0].xy + i * blurSize *" \
+		"			blurMultiplyVec) * incrementalGaussian.x;" \
+		"		coefficientSum += 2 * incrementalGaussian.x;" \
+		"		incrementalGaussian.xy *= incrementalGaussian.yz;" \
+		"	}" \
+		"	gl_FragColor = avgValue / coefficientSum;" \
+		"}";
+		const std::string blend_shader_str_ = "" \
+			"uniform sampler2D scene;" \
+			"uniform sampler2D bloomBlur;" \
+			"uniform float exposure;" \
+			"" \
+			"void main() {" \
+			"	vec3 sceneColor = texture(scene, gl_TexCoord[0].xy).rgb;" \
+			"	vec3 bloomColor = texture(bloomBlur, gl_TexCoord[0].xy).rgb;" \
+			"	sceneColor += bloomColor;" \
+			"	vec3 result = vec3(1.0) - exp(-sceneColor * exposure);" 
+			"	gl_FragColor = vec4(result, 1.0f); }";
+
+
 	};
 }
