97ab7487850afc96cc608f4d9c13c9a6c1f912aa
Peter Repukat <orly.alia5@gmail.com>
2020-01-11 01:00:07 +0100

    temp

--- FSE/FSEObject/ScriptObject.cpp
+++ FSE/FSEObject/ScriptObject.cpp
@@ -1,338 +1,191 @@
-//#include "ScriptObject.h"
-//#include "../Scene.h"
-//#include "../Application.h"
-//
-//#include <regex>
-//
-//namespace fse
-//{
-//	ScriptObject::ScriptObject(const std::string& scriptName) : FSEObject(), script_name_(scriptName)
-//	{
-//		
-//	}
-//
-//	ScriptObject::ScriptObject(const sf::Vector2f& spawnPos, const std::string& scriptName) : FSEObject(spawnPos), script_name_(scriptName)
-//	{
-//	}
-//
-//	void ScriptObject::spawned()
-//	{
-//		try {
-//			auto chai = getChai();
-//			chai->use(script_name_);
-//
-//			std::smatch match;
-//			std::regex_search(script_name_, match, std::regex(R"((?!.*(?=\/|\\)).*(?=\.chai))"));
-//
-//			const std::string classname = match.str();
-//
-//			chai->set_global(chaiscript::var(std::ref(*scene_)), "scriptObjectScene");
-//			script_child_ = chai->eval(std::string(
-//				classname + "(scriptObjectScene.getObjectWithId("+  std::to_string(getID()) + ").lock());"));
-//
-//			initialized_ = true;
-//
-//			if (spawnedFun != nullptr)
-//				spawnedFun(script_child_);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//			FSEObject::destroy();
-//		}
-//	}
-//
-//	void ScriptObject::onDespawn()
-//	{
-//		try {
-//			if (onDespawnFun != nullptr)
-//				onDespawnFun(script_child_);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//	}
-//
-//	void ScriptObject::update(float deltaTime)
-//	{
-//		FSEObject::update(deltaTime);
-//		try {
-//			if (updateFun != nullptr)
-//				updateFun(script_child_, deltaTime);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//	}
-//
-//	void ScriptObject::draw(sf::RenderTarget& target)
-//	{
-//		try
-//		{
-//			if (drawFun != nullptr)
-//				drawFun(script_child_, target);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//	}
-//
-//	void ScriptObject::drawNormals(sf::RenderTarget& target)
-//	{
-//		try {
-//			if (drawNormalsFun != nullptr)
-//				drawNormalsFun(script_child_, target);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//	}
-//
-//	void ScriptObject::drawSpecular(sf::RenderTarget& target)
-//	{
-//		try {
-//			if (drawSpecularFun != nullptr)
-//				drawSpecularFun(script_child_, target);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//	}
-//
-//	void ScriptObject::setPosition(const sf::Vector2f position)
-//	{
-//		try {
-//			if (setPositionFun != nullptr)
-//				setPositionFun(script_child_, position);
-//		}
-//		catch (const chaiscript::exception::eval_error& error) {
-//			std::cerr << error.pretty_print() << '\n';
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//	}
-//
-//	sf::Vector2f ScriptObject::getPosition()
-//	{
-//		try {
-//			if (getPositionFun != nullptr)
-//				return getPositionFun(script_child_);
-//		}
-//		catch (const chaiscript::exception::eval_error& error) {
-//			std::cerr << error.pretty_print() << '\n';
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//		return FSEObject::getPosition();
-//	}
-//
-//	sf::FloatRect ScriptObject::GetAABBs() const
-//	{
-//		try {
-//			if (getAABBsFun != nullptr)
-//				return getAABBsFun(script_child_);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//		return FSEObject::GetAABBs();
-//	}
-//
-//	void ScriptObject::BeginContact(FSEObject* otherObject, b2Contact* contact)
-//	{
-//		try {
-//			if (beginContactFun != nullptr)
-//				beginContactFun(script_child_, otherObject, contact);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//	}
-//
-//	void ScriptObject::EndContact(FSEObject* otherObject, b2Contact* contact)
-//	{
-//		try {
-//			if (endContactFun != nullptr)
-//				endContactFun(script_child_, otherObject, contact);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//	}
-//
-//	void ScriptObject::PreSolve(FSEObject* otherObject, b2Contact* contact, const b2Manifold* oldManifold)
-//	{
-//		try
-//		{
-//			if (preSolveFun != nullptr)
-//				preSolveFun(script_child_, otherObject, contact, oldManifold);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//	}
-//
-//	void ScriptObject::PostSolve(FSEObject* otherObject, b2Contact* contact, const b2ContactImpulse* impulse)
-//	{
-//		try {
-//			if (postSolveFun != nullptr)
-//				postSolveFun(script_child_, otherObject, contact, impulse);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//	}
-//
-//	bool ScriptObject::destroy()
-//	{
-//		try {
-//			if (destroyFun != nullptr)
-//				return destroyFun(script_child_);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//		return FSEObject::destroy();
-//	}
-//
-//	void ScriptObject::BeginContactComponents(FSEObject* otherObject, b2Contact* contact)
-//	{
-//		try {
-//			if (beginContactComponentsFun != nullptr)
-//				beginContactComponentsFun(script_child_, otherObject, contact);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//	}
-//
-//	void ScriptObject::EndContactComponents(FSEObject* otherObject, b2Contact* contact)
-//	{
-//		try {
-//			if (endContactComponentsFun != nullptr)
-//				endContactComponentsFun(script_child_, otherObject, contact);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//	}
-//
-//	void ScriptObject::PreSolveComponents(FSEObject* otherObject, b2Contact* contact, const b2Manifold* oldManifold)
-//	{
-//		try {
-//		if (preSolveComponentsFun != nullptr)
-//			preSolveComponentsFun(script_child_, otherObject, contact, oldManifold);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//	}
-//
-//	void ScriptObject::PostSolveComponents(FSEObject* otherObject, b2Contact* contact, const b2ContactImpulse* impulse)
-//	{
-//		try {
-//		if (postSolveComponentsFun != nullptr)
-//			postSolveComponentsFun(script_child_, otherObject, contact, impulse);
-//		}
-//		catch (const std::exception& e)
-//		{
-//			std::cerr << e.what() << "\n";
-//		}
-//	}
-//
-//
-//	FSE_V8_REGISTER(ScriptObject)
-//	{
-//		RegisterJSUserTypeFromRTTR<ScriptObject>(isolate);
-//		//chai.add(chaiscript::base_class<fse::FSEObject, ScriptObject>());
-//		//chai.add(chaiscript::constructor<ScriptObject(const std::string&)>(), "ScriptObject");
-//		//chai.add(chaiscript::constructor<ScriptObject(const sf::Vector2f&, const std::string&)>(), "ScriptObject");
-//		//chai.add(chaiscript::fun([](const ScriptObject* object)
-//		{
-//			return object->initialized_;
-//		}), "isInitialized");
-//
-//		//chai.add(chaiscript::fun([](const ScriptObject* object)
-//		{
-//			return object->script_name_;
-//		}), "getScriptName");
-//
-//		//chai.add(chaiscript::fun([](const ScriptObject* object)
-//		{
-//			return object->script_child_;
-//		}), "_child");
-//
-//		//chai.add(chaiscript::fun(&ScriptObject::position_), "position");
-//
-//		//chai.add(chaiscript::fun(&ScriptObject::spawnedFun), "spawnedCallback");
-//		//chai.add(chaiscript::fun(&ScriptObject::onDespawnFun), "onDespawnCallback");
-//
-//		//chai.add(chaiscript::fun(&ScriptObject::updateFun), "updateCallback");
-//		//chai.add(chaiscript::fun(&ScriptObject::drawFun), "drawCallback");
-//		//chai.add(chaiscript::fun(&ScriptObject::drawNormalsFun), "drawNormalsCallback");
-//		//chai.add(chaiscript::fun(&ScriptObject::drawSpecularFun), "drawSpecularCallback");
-//
-//		//chai.add(chaiscript::fun(&ScriptObject::setPositionFun), "setPositionCallback");
-//		//chai.add(chaiscript::fun(&ScriptObject::getPositionFun), "getPositionCallback");
-//		//chai.add(chaiscript::fun(&ScriptObject::getAABBsFun), "getAABBsCallback");
-//
-//		//chai.add(chaiscript::fun(&ScriptObject::beginContactFun), "beginContactCallback");
-//		//chai.add(chaiscript::fun(&ScriptObject::endContactFun), "endContactCallback");
-//
-//		//chai.add(chaiscript::fun(&ScriptObject::preSolveFun), "preSolveCallback");
-//		//chai.add(chaiscript::fun(&ScriptObject::postSolveFun), "postSolveCallback");
-//
-//		//chai.add(chaiscript::fun(&ScriptObject::destroyFun), "destroyCallback");
-//
-//		//chai.add(chaiscript::fun(&ScriptObject::beginContactComponentsFun), "beginContactComponentsCallback");
-//		//chai.add(chaiscript::fun(&ScriptObject::endContactComponentsFun), "endContactComponentsCallback");
-//
-//		//chai.add(chaiscript::fun(&ScriptObject::preSolveComponentsFun), "preSolveComponentsCallback");
-//		//chai.add(chaiscript::fun(&ScriptObject::postSolveComponentsFun), "postSolveComponentsCallback");
-//
-//	}
-//
-//}
-//
-//#include <rttr/registration>
-//RTTR_REGISTRATION
-//{
-//	using namespace rttr;
-//	using namespace fse;
-//	registration::class_<ScriptObject>("fse::ScriptObject")
-//		.constructor<const std::string& >()
-//		(
-//			policy::ctor::as_std_shared_ptr,
-//			parameter_names("script name")
-//			)
-//		.constructor<const sf::Vector2f&, const std::string&>()
-//		(
-//			policy::ctor::as_std_shared_ptr,
-//			parameter_names("spawn position", "script name")
-//			)
-//		.property_readonly("script_name_", &ScriptObject::script_name_)
-//		(
-//			metadata("CTOR_ARG", "script name")
-//		)
-//		.property_readonly("initialized_", &ScriptObject::initialized_)
-//		.property_readonly("script_child_", &ScriptObject::script_child_)
-//		;
-//}
+#include "ScriptObject.h"
+#include <rttr/registration>
+
+namespace fse
+{
+	ScriptObject::ScriptObject()
+	{
+	}
+
+	ScriptObject::ScriptObject(v8::Persistent < v8::Object, v8::CopyablePersistentTraits<v8::Object>> object) : ScriptObject({ 0,0 })
+	{
+		child_ = object;
+	}
+
+	ScriptObject::ScriptObject(const sf::Vector2f& spawnPos) : FSEObject(spawnPos)
+	{
+	}
+
+	void ScriptObject::spawned()
+	{
+		auto iso = v8::Isolate::GetCurrent();
+		auto hscope = v8::HandleScope(v8::Isolate::GetCurrent());
+		auto ctx = iso->GetCurrentContext();
+		if (!child_.IsEmpty())
+		{
+			auto meh = child_.Get(iso)->GetPropertyNames(ctx).ToLocalChecked();
+			auto blaaa = v8pp::from_v8<std::vector<std::string>>(iso, meh);
+
+			for (auto fu : blaaa)
+				std::cout << fu << "\n";
+
+			auto getId = child_.Get(iso)->Get(ctx, v8pp::to_v8(iso, "getID"));
+			
+			//auto val = child_->Get(ctx, v8pp::to_v8(iso, "testString"));
+			//if (!val.IsEmpty())
+			//{
+			//	std::string meh = v8pp::from_v8<std::string>(iso, val.ToLocalChecked());
+			//}
+			////if (child_->HasOwnProperty(v8::Isolate::GetCurrent()->GetCurrentContext(),
+			////	v8::Local<v8::Name>(v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), "testString").ToLocalChecked())).FromJust())
+			////{
+			////	std::cout << "test has property!\n";
+			////}
+		}
+	}
+
+	
+	void ScriptObject::onDespawn()
+	{
+	}
+
+	void ScriptObject::update(float deltaTime)
+	{
+		
+	}
+
+	void ScriptObject::draw(sf::RenderTarget& target)
+	{
+	}
+
+	void ScriptObject::drawNormals(sf::RenderTarget& target)
+	{
+	}
+
+	void ScriptObject::drawSpecular(sf::RenderTarget& target)
+	{
+	}
+
+	void ScriptObject::setPosition(const sf::Vector2f position)
+	{
+	}
+
+	sf::Vector2f ScriptObject::getPosition()
+	{
+		return FSEObject::getPosition();
+	}
+
+	sf::FloatRect ScriptObject::GetAABBs() const
+	{
+		return FSEObject::GetAABBs();
+	}
+
+	void ScriptObject::BeginContact(FSEObject* otherObject, b2Contact* contact)
+	{
+	}
+
+	void ScriptObject::EndContact(FSEObject* otherObject, b2Contact* contact)
+	{
+	}
+
+	void ScriptObject::PreSolve(FSEObject* otherObject, b2Contact* contact, const b2Manifold* oldManifold)
+	{
+	}
+
+	void ScriptObject::PostSolve(FSEObject* otherObject, b2Contact* contact, const b2ContactImpulse* impulse)
+	{
+	}
+
+	bool ScriptObject::destroy()
+	{
+		return FSEObject::destroy();
+	}
+
+	void ScriptObject::BeginContactComponents(FSEObject* otherObject, b2Contact* contact)
+	{
+	}
+
+	void ScriptObject::EndContactComponents(FSEObject* otherObject, b2Contact* contact)
+	{
+	}
+
+	void ScriptObject::PreSolveComponents(FSEObject* otherObject, b2Contact* contact, const b2Manifold* oldManifold)
+	{
+	}
+
+	void ScriptObject::PostSolveComponents(FSEObject* otherObject, b2Contact* contact, const b2ContactImpulse* impulse)
+	{
+	}
+
+	FSE_V8_REGISTER(ScriptObject)
+	{
+
+		v8::HandleScope handle_scope(isolate);
+		v8pp::class_<ScriptObject, v8pp::shared_ptr_traits>ScriptObject_class(isolate);
+		ScriptObject_class.inherit<FSEObject>();
+		fse::addV8DownCastHelper<fse::FSEObject, fse::ScriptObject>();
+		ScriptObject_class.auto_wrap_objects(true);
+		ScriptObject_class.ctor<>(
+			[](v8::FunctionCallbackInfo<v8::Value> const& args)
+			{
+				auto iso = v8::Isolate::GetCurrent();
+				auto ctx = iso->GetCurrentContext();
+				// TODO: overloads
+				auto test = args.This();
+				//auto test = args.Data();
+				v8::Local<v8::Object> v8Object =
+					test->ToObject(args.GetIsolate()->GetCurrentContext()).ToLocalChecked();
+				if (test->IsObject())
+				{
+					std::cout << "test is object\n";
+					auto meh = v8Object->GetPropertyNames(ctx).ToLocalChecked();
+					auto blaaa = v8pp::from_v8<std::vector<std::string>>(iso, meh);
+
+					for (auto fu : blaaa)
+						std::cout << fu << "\n";
+				}
+				v8::Persistent<v8::Object, v8::CopyablePersistentTraits<v8::Object>> persistentThis(iso, v8Object);
+
+				return std::make_shared<ScriptObject>(persistentThis);
+			});
+		ScriptObject_class.function("setScriptChild", [](v8::FunctionCallbackInfo<v8::Value> const& args)
+		{
+				auto iso = v8::Isolate::GetCurrent();
+				auto ctx = iso->GetCurrentContext();
+				// TODO: overloads
+				auto test = args[0];
+				v8::Local<v8::Object> v8Object =
+					test->ToObject(args.GetIsolate()->GetCurrentContext()).ToLocalChecked();
+				if (test->IsObject())
+				{
+					std::cout << "test is object\n";
+					auto meh = v8Object->GetPropertyNames(ctx).ToLocalChecked();
+					auto blaaa = v8pp::from_v8<std::vector<std::string>>(iso, meh);
+
+					for (auto fu : blaaa)
+						std::cout << fu << "\n";
+				}
+				v8::Persistent<v8::Object, v8::CopyablePersistentTraits<v8::Object>> persistentThis(iso, v8Object);
+				v8pp::from_v8<std::shared_ptr<fse::ScriptObject>>(iso, args.This())->child_ = persistentThis;
+		});
+		module.class_("ScriptObject", ScriptObject_class);
+	}
+	
+}
+
+RTTR_REGISTRATION
+{
+	using namespace rttr;
+
+	registration::class_<fse::ScriptObject>("fse::ScriptObject")
+	//.constructor<>()
+	//(
+	//policy::ctor::as_std_shared_ptr
+	//)
+	//.constructor<const sf::Vector2f&>()
+	//(
+	//	policy::ctor::as_std_shared_ptr,
+	//	parameter_names("spawn position")
+	//)
+	;
+}
--- FSE/FSEObject/ScriptObject.h
+++ FSE/FSEObject/ScriptObject.h
@@ -1,84 +1,55 @@
-//#pragma once
-//#include "FSEObject.h"
-//
-//namespace fse
-//{
-//	class ScriptObject : public FSEObject
-//	{
-//	public:
-//		explicit ScriptObject(const std::string& scriptName);
-//		explicit ScriptObject(const sf::Vector2f& spawnPos, const std::string& scriptName);
-//		~ScriptObject() = default;
-//
-//		void spawned() override;
-//		void onDespawn() override;
-//
-//		void update(float deltaTime) override;
-//
-//		void draw(sf::RenderTarget& target) override;
-//		void drawNormals(sf::RenderTarget& target) override;
-//		void drawSpecular(sf::RenderTarget& target) override;
-//
-//		void setPosition(const sf::Vector2f position) override;
-//		sf::Vector2f getPosition() override;
-//		sf::FloatRect GetAABBs() const override;
-//
-//		void BeginContact(FSEObject* otherObject, b2Contact* contact) override;
-//		void EndContact(FSEObject* otherObject, b2Contact* contact) override;
-//		void PreSolve(FSEObject* otherObject, b2Contact* contact, const b2Manifold* oldManifold) override;
-//		void PostSolve(FSEObject* otherObject, b2Contact* contact, const b2ContactImpulse* impulse) override;
-//
-//		bool destroy() override;
-//	protected:
-//		void BeginContactComponents(FSEObject* otherObject, b2Contact* contact) override;
-//		void EndContactComponents(FSEObject* otherObject, b2Contact* contact) override;
-//		void PreSolveComponents(FSEObject* otherObject, b2Contact* contact, const b2Manifold* oldManifold) override;
-//		void PostSolveComponents(FSEObject* otherObject, b2Contact* contact, const b2ContactImpulse* impulse) override;
-//
-//	private:
-//
-//		std::string script_name_;
-//		bool initialized_ = false;
-//		chaiscript::Boxed_Value script_child_;
-//
-//
-//		mutable std::function<void(const chaiscript::Boxed_Value&)> spawnedFun = nullptr;
-//		mutable std::function<void(const chaiscript::Boxed_Value&)> onDespawnFun = nullptr;
-//
-//
-//		mutable std::function<void(const chaiscript::Boxed_Value&, float)> updateFun = nullptr;
-//		mutable std::function<void(const chaiscript::Boxed_Value&, sf::RenderTarget&)> drawFun = nullptr;
-//		mutable std::function<void(const chaiscript::Boxed_Value&, sf::RenderTarget&)> drawNormalsFun = nullptr;
-//		mutable std::function<void(const chaiscript::Boxed_Value&, sf::RenderTarget&)> drawSpecularFun = nullptr;
-//
-//
-//		mutable std::function<void(const chaiscript::Boxed_Value&, const sf::Vector2f)> setPositionFun = nullptr;
-//		mutable std::function<sf::Vector2f(const chaiscript::Boxed_Value&)> getPositionFun = nullptr;
-//		mutable std::function<sf::FloatRect(const chaiscript::Boxed_Value&)> getAABBsFun = nullptr;
-//
-//
-//		mutable std::function<void(const chaiscript::Boxed_Value&, FSEObject*, b2Contact*)> beginContactFun = nullptr;
-//		mutable std::function<void(const chaiscript::Boxed_Value&, FSEObject*, b2Contact*)> endContactFun = nullptr;
-//
-//		mutable std::function<void(const chaiscript::Boxed_Value&, FSEObject*, b2Contact*, const b2Manifold*)> preSolveFun = nullptr;
-//		mutable std::function<void(const chaiscript::Boxed_Value&, FSEObject*, b2Contact*, const b2ContactImpulse*)> postSolveFun = nullptr;
-//
-//		mutable std::function<bool(const chaiscript::Boxed_Value&)> destroyFun = nullptr;
-//
-//
-//		mutable std::function<void(const chaiscript::Boxed_Value&, FSEObject*, b2Contact*)> beginContactComponentsFun = nullptr;
-//		mutable std::function<void(const chaiscript::Boxed_Value&, FSEObject*, b2Contact*)> endContactComponentsFun = nullptr;
-//
-//		mutable std::function<void(const chaiscript::Boxed_Value&, FSEObject*, b2Contact*, const b2Manifold*)> preSolveComponentsFun = nullptr;
-//		mutable std::function<void(const chaiscript::Boxed_Value&, FSEObject*, b2Contact*, const b2ContactImpulse*)> postSolveComponentsFun = nullptr;
-//
-//		FSE_V8_ENABLE(ScriptObject);
-//		FSE_V8_REGISTRATION_FRIEND
-//
-//		RTTR_ENABLE(fse::FSEObject)
-//		RTTR_REGISTRATION_FRIEND
-//
-//	};
-//
-//
-//}
+#pragma once
+#include "FSEObject.h"
+
+namespace fse
+{
+	class ScriptObject : public FSEObject
+	{
+	public:
+		explicit ScriptObject();
+		explicit ScriptObject(v8::Persistent < v8::Object, v8::CopyablePersistentTraits<v8::Object>> object);
+		explicit ScriptObject(const sf::Vector2f& spawnPos);
+		~ScriptObject() = default;
+
+		void spawned() override;
+		void onDespawn() override;
+
+		void update(float deltaTime) override;
+
+		void draw(sf::RenderTarget& target) override;
+		void drawNormals(sf::RenderTarget& target) override;
+		void drawSpecular(sf::RenderTarget& target) override;
+
+		void setPosition(const sf::Vector2f position) override;
+		sf::Vector2f getPosition() override;
+		sf::FloatRect GetAABBs() const override;
+
+		void BeginContact(FSEObject* otherObject, b2Contact* contact) override;
+		void EndContact(FSEObject* otherObject, b2Contact* contact) override;
+		void PreSolve(FSEObject* otherObject, b2Contact* contact, const b2Manifold* oldManifold) override;
+		void PostSolve(FSEObject* otherObject, b2Contact* contact, const b2ContactImpulse* impulse) override;
+
+		bool destroy() override;
+	protected:
+		void BeginContactComponents(FSEObject* otherObject, b2Contact* contact) override;
+		void EndContactComponents(FSEObject* otherObject, b2Contact* contact) override;
+		void PreSolveComponents(FSEObject* otherObject, b2Contact* contact, const b2Manifold* oldManifold) override;
+		void PostSolveComponents(FSEObject* otherObject, b2Contact* contact, const b2ContactImpulse* impulse) override;
+
+	private:
+
+		std::string script_name_;
+		bool initialized_ = false;
+
+		v8::Persistent < v8::Object, v8::CopyablePersistentTraits<v8::Object>> child_;
+
+		FSE_V8_ENABLE(ScriptObject);
+		FSE_V8_REGISTRATION_FRIEND
+
+		RTTR_ENABLE(fse::FSEObject)
+		RTTR_REGISTRATION_FRIEND
+
+	};
+
+
+}
